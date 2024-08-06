538a446f17ad37ef5d46c4a5fd93280c6838c427
Mathew Tran <mathewtrangamedesign@gmail.com>
2024-08-05 19:52:27 -0700

    add small anim when switching between flowers in book

--- Scripts/UI/Screens/PlantBook/PlantBookInfo.gd
+++ Scripts/UI/Screens/PlantBook/PlantBookInfo.gd
@@ -5,6 +5,11 @@ extends Panel
 @onready var FlowerDescription = $Panel2/FlowerDescription
 
 func ShowPlantData(plantData : PlantData):
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(1,0), .2)
+	await tween.finished
 	FlowerImage.texture = plantData.PlantPicture
 	FlowerTitle.text = plantData.FlowerName.to_upper()
 	FlowerDescription.text = plantData.FlowerDescription
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property(self, "scale", Vector2.ONE, .2)
