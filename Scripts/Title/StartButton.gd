2ed8385a9aebb95faefbf791fc64eac58819f0b4
Mathew Tran <mathew.tran.dev@gmail.com>
2024-08-05 21:22:16 -0700

    speed up start button press

--- Scripts/Title/StartButton.gd
+++ Scripts/Title/StartButton.gd
@@ -6,10 +6,11 @@ func _on_button_up():
 	$AudioStreamPlayer2D.play()
 	self_modulate = Color.DARK_GRAY
 	disabled = true
-
-func _on_audio_stream_player_2d_finished():
+	var timer = get_tree().create_timer(.2)
+	await timer.timeout
 	if Game.bHasDoneTutorial:
 		get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 	else:
 		get_tree().change_scene_to_file("res://Scenes/Tutorial.tscn")
 		Game.bHasDoneTutorial = true
+
