d727381f8b8aa53840eb83ef173f10c7333790cd
Mathew Tran <mathew.tran.dev@gmail.com>
2024-08-09 23:29:26 -0700

    reset shake

--- Scripts/UI/PlantBookButton.gd
+++ Scripts/UI/PlantBookButton.gd
@@ -12,6 +12,8 @@ func _on_animation_player_animation_finished(anim_name):
 	AmountToShake -= 1
 	if AmountToShake > 0:
 		$AnimationPlayer.play("animate")
+	else:
+		AmountToShake = 5
 
 
 func _on_animation_player_animation_started(anim_name):
