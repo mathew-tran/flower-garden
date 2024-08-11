9784d91d76dfdac089c361580649e9929b1d9d48
Mathew Tran <mathew.tran.dev@gmail.com>
2024-08-11 06:03:47 -0700

    animate plant book in

--- Scripts/UI/Screens/PlantBook/PlantBookUI.gd
+++ Scripts/UI/Screens/PlantBook/PlantBookUI.gd
@@ -4,9 +4,11 @@ extends Panel
 @onready var PlantContainer = $Panel/Panel/ScrollContainer/VBoxContainer
 
 var bIsSetup = false
+var bIsAnimating = false
 
 func _ready():
 	PopulatePlantData()
+	position = Vector2(-2000,0)
 	
 func PopulatePlantData():
 	for child in PlantContainer.get_children():
@@ -30,21 +32,44 @@ func OnPlantDataSelected(plantData : PlantData):
 
 
 func _on_plant_book_button_button_up():
+	if bIsAnimating:
+		return
+		
 	visible = true
 
 func _on_close_button_button_up():
+	Close()
+	
+func Close():
+	if bIsAnimating:
+		return
+	bIsAnimating = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", Vector2(-2000,0), .5)
+	await tween.finished
 	visible = false
+	bIsAnimating = false
+	
 	
 func _input(event):
 	if event.is_action_pressed("escape"):
 		if visible:
-			visible = false
+			Close()
 
+func _process(delta):
+	print(position)
+	
 func _on_visibility_changed():
 	if is_visible_in_tree():
 		if visible:
 			for child in PlantContainer.get_children():
 				child.Update()
 			InputManager.ShowMouse.emit(false)
+			bIsAnimating = true
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "position", Vector2.ZERO, .5)
+			tween.set_trans(Tween.TRANS_EXPO)
+			await tween.finished
+			bIsAnimating = false
 	else:
 		InputManager.ShowMouse.emit(true)
